518836e5478e5243683954564917f15641135670
jason13official <jason13official@gmail.com>
2024-08-27 19:43:13 -0400

    fix: update mc and loader versions

--- common/src/main/java/com/cursee/new_shield_variants/Constants.java
+++ common/src/main/java/com/cursee/new_shield_variants/Constants.java
@@ -9,8 +9,8 @@ public class Constants {
 
 	public static final String MOD_ID = "new_shield_variants";
 	public static final String MOD_NAME = "New Shield Variants";
-	public static final String MOD_VERSION = "1.3.0";
-	public static final String MC_VERSION_RAW = "[1.21]";
+	public static final String MOD_VERSION = "1.3.1";
+	public static final String MC_VERSION_RAW = "[1.21.1]";
 	public static final Pair<String, String> PUBLISHER_AUTHOR = new Pair<String, String>("Lupin", "Jason13");
 	public static final Triplet<String, String, String> PRIMARY_CURSEFORGE_MODRINTH = new Triplet<String, String, String>("https://www.curseforge.com/members/lupin/projects", "https://www.curseforge.com/members/lupin/projects", "https://modrinth.com/user/Lupin/mods");
 	public static final Logger LOG = LoggerFactory.getLogger(MOD_NAME);
