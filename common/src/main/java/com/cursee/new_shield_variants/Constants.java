6668df79f880f5520c3e9bd2dd88d347cdd62fbb
jason13official <jason13official@gmail.com>
2024-08-27 20:08:09 -0400

    fix: unrestrict mc version, bump mod version

--- common/src/main/java/com/cursee/new_shield_variants/Constants.java
+++ common/src/main/java/com/cursee/new_shield_variants/Constants.java
@@ -9,7 +9,7 @@ public class Constants {
 
 	public static final String MOD_ID = "new_shield_variants";
 	public static final String MOD_NAME = "New Shield Variants";
-	public static final String MOD_VERSION = "1.3.0";
+	public static final String MOD_VERSION = "1.3.1";
 	public static final String MC_VERSION_RAW = "[1.20.1]";
 	public static final Pair<String, String> PUBLISHER_AUTHOR = new Pair<String, String>("Lupin", "Jason13");
 	public static final Triplet<String, String, String> PRIMARY_CURSEFORGE_MODRINTH = new Triplet<String, String, String>("https://www.curseforge.com/members/lupin/projects", "https://www.curseforge.com/members/lupin/projects", "https://modrinth.com/user/Lupin/mods");
